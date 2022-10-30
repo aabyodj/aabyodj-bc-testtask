0fedac4a2aff1052cb7f32718d98c18f42b401bb
aabyodj <aabyodj@gmail.com>
2022-10-30 12:52:52 +0300

    Initial commit

---
+++ src/main/java/bootcamp/testtask/BootcampTesttaskApplication.java
@@ -0,0 +1,13 @@
+package bootcamp.testtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampTesttaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampTesttaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bootcamp/testtask/BootcampTesttaskApplicationTests.java
@@ -0,0 +1,13 @@
+package bootcamp.testtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampTesttaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
