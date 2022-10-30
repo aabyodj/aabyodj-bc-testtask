ddddba9adde80e1e5e28ce1edad99adae8dd68ff
aabyodj <aabyodj@gmail.com>
2022-10-30 13:15:32 +0300

    Add User entity

---
+++ repository/src/main/java/bootcamp/testtask/repository/User.java
@@ -0,0 +1,53 @@
+package bootcamp.testtask.repository;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "surname", length = 40, nullable = false)
+    private String surname;
+
+    @Column(name = "name", length = 20, nullable = false)
+    private String name;
+
+    @Column(name = "patronymic", length = 40, nullable = false)
+    private String patronymic;
+
+    @Column(name = "email", length = 50, nullable = false)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", length = 16, nullable = false)
+    private Role role;
+
+    @Getter
+    @RequiredArgsConstructor
+    public enum Role {
+        ADMIN("Administrator"), 
+        SALES("Sale User"), 
+        CUSTOMER("Customer User"), 
+        SECURE("Secure API User");
+        
+        private final String longName;
+    }
+
+}
